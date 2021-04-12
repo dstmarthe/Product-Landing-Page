51897d74bcfeee14fbfac923ae6d01394d4cbf32
Dale <72896959+dstmarthe@users.noreply.github.com>
2021-04-12 00:50:08 -0400

    header gradient and features animation

--- script.js
+++ script.js
@@ -1,11 +1,26 @@
 window.onscroll = function () {
-	scrollNav();
+  scrollNav(), scrollFeatures();
 };
 
 function scrollNav() {
-	if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
-		document.getElementById("nav-bar").className = "bar";
-	} else {
-		document.getElementById("nav-bar").className = "";
-	}
+  if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
+    document.getElementById("nav-bar").className = "bar";
+  } else {
+    document.getElementById("nav-bar").className = "";
+  }
+}
+
+function scrollFeatures() {
+  if (
+    document.body.scrollTop > 500 ||
+    document.documentElement.scrollTop > 500
+  ) {
+    document.getElementById("feat1").className = "ft1";
+    document.getElementById("feat2").className = "ft2";
+    document.getElementById("feat3").className = "ft3";
+  } else {
+    document.getElementById("feat1").className = "invis";
+    document.getElementById("feat2").className = "invis";
+    document.getElementById("feat3").className = "invis";
+  }
 }
